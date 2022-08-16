a5473414788d1cc9de33742ed104f21f8908e078
Denver <dosborn@redhat.com>
2022-08-15 20:52:09 -0400

    Update ssh-keys.sh

--- ssh-keys.sh
+++ ssh-keys.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-mkdir -pv $HOME/ssh-keys
+PATH=/bin:/sbin:/usr/bin:/usr/sbin
 
-ssh-keygen -b 2048 -t rsa -f $HOME/ssh-keys/utility -q -N ""
+# Location of the user's ssh keys (according to the running sshd_config)
+AUTHKEYS=~/$(sshd -T|awk '/authorizedkeysfile/{print $2}')
+KEYDIR=${AUTHKEYS%/*}
 
-cat $HOME/ssh-keys/utility.pub >> $HOME/.ssh/authorized_keys
+# Location of the container's ssh keys
+UTILKEYDIR=~/ssh-keys
+PRVKEY=${UTILKEYDIR}/utility
+PUBKEY=${PRVKEY}.pub
 
+# Create dirs if they do not exist, then generate the ssh kay pair used by the utility container
+# Note: this permits the root user in the container login ia ssh-key to the host.
+test -d ${KEYDIR} || mkdir -pv ${KEYDIR} && chmod 700 ${KEYDIR}
+test -d ${UTILKEYDIR} || mkdir -pv ${UTILKEYDIR} && chmod 700 ${UTILKEYDIR}
+test -f ${PRVKEY} || ssh-keygen -b 2048 -t rsa -f ${PRVKEY} -q -N "" && cat ${PUBKEY} >> ${AUTHKEYS}
